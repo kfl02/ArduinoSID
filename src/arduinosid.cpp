c6b21eff339ca0928c188bcd6c7745ac98202bf1
Karsten Fleischer <fleischer.karsten@gmail.com>
2019-11-28 22:27:31 +0100

    initialization, pull CS line late

--- src/arduinosid.cpp
+++ src/arduinosid.cpp
@@ -11,9 +11,9 @@ auto &ringBuffer = sidArray.getRingBuffer();
 void writeRegister(uint8_t sid, uint8_t reg, uint8_t val) {
     // TODO: somehow wait for last write to be done
 
-    // pull chip select line
+    // reset chip select lines
     for(uint8_t i = 0; i < SIDArray::MAX_NUM_SIDS; i++) {
-        digitalWrite(SID_CS[i], (i == sid) ? LOW : HIGH);
+        digitalWrite(SID_CS[i], HIGH);
     }
 
     // set address lines
@@ -26,26 +26,30 @@ void writeRegister(uint8_t sid, uint8_t reg, uint8_t val) {
         digitalWrite(SID_DX[i], (dx & 1) ? HIGH : LOW);
     }
 
+    // pull chip select line for given SID
+    digitalWrite(SID_CS[sid], LOW);
+
     // TODO: somehow signal everything is ready to write
 }
 
 void setup() {
-    pinMode(SID_A0, OUTPUT);
-    pinMode(SID_A1, OUTPUT);
-    pinMode(SID_A2, OUTPUT);
-    pinMode(SID_A3, OUTPUT);
-    pinMode(SID_A4, OUTPUT);
+    // address lines
+    for(uint8_t i = 0; i < NUM_SID_AX; i++) {
+        pinMode(SID_AX[i], OUTPUT);
+    }
 
-    pinMode(SID_D0, OUTPUT);
-    pinMode(SID_D1, OUTPUT);
-    pinMode(SID_D2, OUTPUT);
-    pinMode(SID_D3, OUTPUT);
-    pinMode(SID_D4, OUTPUT);
-    pinMode(SID_D5, OUTPUT);
-    pinMode(SID_D6, OUTPUT);
-    pinMode(SID_D7, OUTPUT);
+    // data lines
+    for(uint8_t i = 0; i < NUM_SID_DX; i++) {
+        pinMode(SID_DX[i], OUTPUT);
+    }
 
+    // clock line
     pinMode(SID_PHI_2, OUTPUT);
+
+    // chip select lines
+    for(uint8_t i = 0; i < SIDArray::MAX_NUM_SIDS; i++) {
+        pinMode(SID_CS[i], OUTPUT);
+    }
 }
 
 void loop() {
